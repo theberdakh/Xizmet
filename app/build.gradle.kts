266f7df8de9255f5434858d89e0e76946c18f2ed
theberdakh <theberdakh@proton.me>
2025-01-05 12:15:38 +0500

    add Firebase dependencies in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,10 @@ android {
 
 dependencies {
 
+    //Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
